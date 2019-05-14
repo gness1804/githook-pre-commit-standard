2073cf9483bff8c9138ad42efbb11a9bbe7fd6d1
gness1804 <graham.nessler@gmail.com>
2019-05-13 21:10:58 -0500

    Greatly improve preCommit.js

--- preCommit.js
+++ preCommit.js
@@ -4,7 +4,12 @@ const { exec } = require('child_process');
 
 const promisifiedExec = util.promisify(exec);
 const promisifiedReadFile = util.promisify(readFile);
+const forbiddenBranches = ['master', 'develop'];
 
+/**
+ *
+ * @param {string} message - the error message to display
+ */
 const resetAndExit = message => {
   promisifiedExec('git reset HEAD .')
     .then(() => {
@@ -15,27 +20,43 @@ const resetAndExit = message => {
     });
 };
 
-const forbiddenBranches = ['master', 'develop'];
-
+/**
+ * @returns {Promise<Function>} - promise that resolves into child process call
+ */
 const getCurrentBranch = () => promisifiedExec('git rev-parse --abbrev-ref HEAD');
 
-const getListOfChangedFiles = () => promisifiedExec('git diff --name-only');
+/**
+ * @returns {Promise<Function>} - promise that resolves into child process call
+ */
+const getListOfChangedFiles = () => promisifiedExec('git diff HEAD --name-only');
 
-const checkFile = async (data) => {
+/**
+ *
+ * @param {string} data - the contents of the file to check against
+ * @returns {Promise<Function | void>} - promise that resolves or that rejects into a function
+ * that exists the program
+ */
+const checkFile = data => new Promise((resolve, reject) => {
   if (data.match(/debugger/g) && data.match(/debugger/g).length) {
-    resetAndExit('Error: debugger. Exiting.');
+    reject(resetAndExit('Error: debugger. Exiting.'));
   }
   if (data.match(/console/g) && data.match(/console/g).length) {
-    resetAndExit('Error: console statement. Exiting.');
+    reject(resetAndExit('Error: console statement. Exiting.'));
   }
   if (data.match(/it.only/g) && data.match(/it.only/g).length) {
-    resetAndExit('Error: it.only. Exiting.');
+    reject(resetAndExit('Error: it.only. Exiting.'));
   }
   if (data.match(/revert/g) && data.match(/revert/g).length) {
-    resetAndExit('Error: revert. Exiting.');
+    reject(resetAndExit('Error: revert. Exiting.'));
+  } else {
+    resolve();
   }
-};
+});
 
+/**
+ *
+ * @param {string[]} files - array of strings corresponding to file names
+ */
 const loopThroughFiles = files => {
   for (const file of files) {
     if (file) {
@@ -44,28 +65,36 @@ const loopThroughFiles = files => {
   }
 };
 
+/**
+ * @returns {Promise<Function | void>} - promise that resolves into nothing or a function to go through any changed files
+ */
 const checkForWarnings = () => getListOfChangedFiles()
   .then(data => data.stdout.split('\n'))
   .then(files => loopThroughFiles(files));
 
+/* eslint-disable indent */
 getCurrentBranch()
-  .then(ret => {
-    const currentBranch = ret.stdout.trim().replace('\n', '');
-    return forbiddenBranches.find(branch => branch === currentBranch);
-  })
-  .then((ret) => ret ? resetAndExit(`Oops, you are on the ${ret} branch. Please switch branches before you commit.`) : undefined)
-  .then(() => {
-    checkForWarnings();
-  })
-  .then(() => promisifiedExec('npm run prettier'))
-  .then(() => promisifiedExec('npm run lint'))
-  .then(() => promisifiedExec('npm run stylelint'))
-  .then(() => promisifiedExec('npm run test'))
-  .then(() => {
-    process.stdout.write('Sucessfully passed pre-commit checks.');
-    process.exit(0);
-  })
-  .catch(err => {
-    const errorMessage = `Error: ${err.message || JSON.stringify(err)}`;
-    resetAndExit(errorMessage);
-  });
+    .then(ret => {
+      const currentBranch = ret.stdout.trim().replace('\n', '');
+      return forbiddenBranches.find(branch => branch === currentBranch);
+    })
+    .then(ret => ret
+            ? resetAndExit(
+            `Oops, you are on the ${ret} branch. Please switch branches before you commit.`,
+            )
+            : undefined,
+    )
+    .then(() => checkForWarnings())
+    .then(() => promisifiedExec('npm run prettier'))
+    .then(() => promisifiedExec('npm run lint'))
+    .then(() => promisifiedExec('npm run stylelint'))
+    .then(() => promisifiedExec('npm run test'))
+    .then(() => {
+      process.stdout.write('Sucessfully passed pre-commit checks.');
+      process.exit(0);
+    })
+    .catch(err => {
+      const errorMessage = `Error: ${err.message || JSON.stringify(err)}`;
+      resetAndExit(errorMessage);
+    });
+/* eslint-enable indent */
