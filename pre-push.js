d77ac7a3fe4e9a2c896e34615bac5d17f0a37e30
Graham Nessler <grahamnessler@insiderx.com>
2020-06-04 15:18:53 -0500

    Add pre-push

---
+++ pre-push.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+const { exec } = require('child_process');
+/* eslint-disable */
+const util = require('util');
+
+const promisifiedExec = util.promisify(exec);
+
+const getCurrentBranch = () => promisifiedExec('git rev-parse --abbrev-ref HEAD');
+
+getCurrentBranch().then(res => {
+	const regex = /(^wip:?-)|(^exp-)/i;
+	if (regex.test(res.stdout)) {
+		console.error('Error: cannot push branches starting with "wip" or "exp".')
+		process.exit(1);
+	} else {
+		console.info('Successfully passed pre-push tests.');
+		process.exit(0);
+	}
+})
