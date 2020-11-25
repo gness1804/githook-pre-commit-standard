6ee4f7973f0356715f80a2fd1d31142e58613d2d
gness1804 <graham.nessler@gmail.com>
2020-11-25 16:38:53 -0600

    Refactor eslint setup to be simpler and use typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,36 +1,17 @@
-
-
-// https://eslint.org/docs/user-guide/configuring
-
 module.exports = {
   root: true,
-  parser: 'babel-eslint',
-  parserOptions: {
-    sourceType: 'module'
+  "env": {
+    "node" : true
   },
-  env: {
-    browser: true,
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+  ],
+  extends: [
+    'eslint:recommended',
+  ],
+  rules: {
+    '@typescript-eslint/ban-types': 'warn',
+    '@typescript-eslint/no-unused-vars': 'error',
   },
-  extends: ['airbnb-base'],
-  'rules': {
-    'no-console': 'error',
-    'func-names': 'off',
-    'object-shorthand': 'off',
-    'no-alert': 'off',
-    'global-require': 'off',
-    'flowtype-errors/show-errors': 'off',
-    'no-plusplus': 'off',
-    'max-len': 'off',
-    'no-restricted-syntax': 'off',
-    'no-underscore-dangle': 'off',
-    'no-unused-vars': 'error',
-    'arrow-parens': 'off',
-    'operator-linebreak': 'off',
-    'implicit-arrow-linebreak': 'error',
-    'function-paren-newline': 'off',
-    'no-confusing-arrow': 'off',
-    // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
-  }
-}
-
+};
