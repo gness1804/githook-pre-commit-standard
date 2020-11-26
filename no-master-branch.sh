b4bf28a349bb6ea12ab4ce10c9e3f91a9d6639b0
gness1804 <graham.nessler@gmail.com>
2020-11-26 11:39:17 -0600

    Change build dir to dist and add noConsole

--- no-master-branch.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-if test "$(git rev-parse --abbrev-ref HEAD)" == "master"; then
-  echo "Oops, you are on the master branch. Please switch branches before you commit." >&2
-  exit 1
-fi
