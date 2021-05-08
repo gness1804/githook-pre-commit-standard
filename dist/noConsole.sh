d7222e0102c4de5d1fc161ec4faa6b76e57d2370
gness1804 <graham.nessler@gmail.com>
2021-05-08 16:11:30 -0500

    Refactor noConsole

--- dist/noConsole.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if git diff --staged  -- . ':(exclude)package.json' | grep console >/dev/null; then
-   echo "Oops! You have a console statement in your code. Please remove it."
-   echo $(git diff | grep console)
-   exit 1
-fi
