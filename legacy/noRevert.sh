afb66faccc61bcb0a3fea0fc563680c2a89619c1
gness1804 <graham.nessler@gmail.com>
2021-05-10 17:20:42 -0500

    restore the legacy bash files

---
+++ legacy/noRevert.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+if git diff --staged  -- . ':(exclude)package.json' | grep 'revert' >/dev/null; then
+   echo "Oops! You have a revert statement in your code. Please remove it."
+   echo $(git diff | grep 'revert')
+   exit 1
+fi
